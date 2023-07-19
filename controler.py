b76a652565565c0db47e251538f300052418aba4
luccaz <luccazoppi@gmail.com>
2023-07-18 22:07:46 -0300

    i think i've fixed it

--- controler.py
+++ controler.py
@@ -2,6 +2,7 @@ import control.matlab as mat
 import json
 from plant import Plant
 from requirements import Requirements
+from numpy import sqrt, pi, sin, angle, roots
 
 class Controler:
     def __init__(self, filepath: str, dt: float) -> None:
@@ -14,8 +15,10 @@ class Controler:
 
     def generate_tfs(self) -> None:
         self.lead = mat.tf([self.T, 1.], [self.alpha*self.T, 1.0])
-        self.P = mat.tf([self.Kp], [1.])
-        self.C = self.lead*self.P
+        self.C = self.lead*self.Kp
+
+    def get_gains(self) -> tuple:
+        return tuple([getattr(self, g) for g in self.gains])
 
     def update(self, new_gains: dict)-> None:
         for k in self.gains:
@@ -32,5 +35,23 @@ class Controler:
         with open(filepath,"w") as f:
             json.dump({k: d[k] for k in self.gains}, f)
 
+    # consertar isso?
     def project_analytical(self, plant: Plant, reqs: Requirements) -> None:
-        pass
+        wb = reqs.wb
+        PM = reqs.pm
+        J = plant.J
+        R = plant.R
+        m = plant.m
+        b = plant.b
+        Jeq = J + m*R**2
+        x = roots([1, 2*Jeq*wb ,-(Jeq*wb**2)**2-(wb*b)**2])
+        x.sort()
+        self.Kp = x[1]
+        y = roots([Jeq**2, b**2, -self.Kp**2])
+        y.sort()
+        wcp = sqrt(y[1])
+        PMa=angle(self.Kp/(-Jeq*wcp**2+b*1j*wcp))+pi
+        phimax=PM*pi/180-PMa
+        self.alpha=(1-sin(phimax))/(1+sin(phimax))
+        self.T = 1/(wcp*sqrt(self.alpha))
+        self.generate_tfs()
--- main.py
+++ main.py
@@ -9,25 +9,36 @@ import control.matlab as mat
 import matplotlib.pyplot as plt
 import numpy as np
 
-c = Controler("gains.json", 1e-4)
-plant = Plant(1.0, 1.0, 1.0, 0.01)
-sys = System(c, plant)
-req = Requirements(wb = 2000, pm = 60, err = 0.0)
-opt_cma = Optimizer(lambda J, x, sig: cma.fmin2(J, x, sig, incpopsize= 5), [(1.0, 0.2, 0.3, 0.5), 10.0])
-opt_cma.minimize(req, sys)
-print(mat.margin(sys.Ga)[1])
-print(mat.bandwidth(sys.Gf))
-# opt_nelder_mead = Optimizer(lambda f, x: minimize(f, x, method='Nelder-Mead'), [(1.0, 0.2, 0.3, 0.5)])
-# opt_nelder_mead.minimize(req, sys)
-# print(mat.margin(sys.Ga)[1])
-# print(mat.bandwidth(sys.Gf))
-y, t =  mat.step(sys.Gf, T = np.linspace(0.,0.1,1000))
-print(mat.pole(sys.Gf))
-print(mat.zero(sys.Gf))
-print(sys.Gf)
-print("Kp: ", c.Kp)
-print("alpha: ",c.alpha)
-print("T: ", c.T)
-plt.plot(t, y)
-plt.show()
+ctrl = Controler("initial.json", 1e-4)
+plant = Plant(1., 1.0, 5.0, 0.001)
+sys = System(ctrl, plant)
+
+def nelder_mead_opt(f, *args):
+    return minimize(f, *args, method='Nelder-Mead', bounds=[(0., None), (0.0, 1.0), (0.0, None)])
+
+def cma_es_opt(f, *args):
+    return cma.fmin2(f, *args, options = {"bounds" : [[0., 0., 0.], [None, 1., None]], "verbose" : -2})
+
+def benchmark(algo: str, wb: float, pm: float):
+    reqs = Requirements(wb = wb, pm = pm, err = 0.0)
+    ctrl.project_analytical(plant, reqs)
+    if algo == 'cma':
+        opt = Optimizer(cma_es_opt, [ctrl.get_gains(), 100])
+    elif algo == 'nelder-mead':
+        opt = Optimizer(nelder_mead_opt, [ctrl.get_gains()])
+    else:
+        raise ValueError("Invalid algorithm name")
+    req = Requirements(wb = wb, pm = pm, err = 0.0)
+    opt.minimize(req, sys)
+    print(mat.margin(sys.Ga)[1])
+    print(mat.bandwidth(sys.Gf))
+    print("Kp: ", ctrl.Kp)
+    print("alpha: ",ctrl.alpha)
+    print("T: ", ctrl.T)
+    y, t =  mat.step(sys.Gf, T = np.linspace(0.,0.7,1000))
+    plt.plot(t, y)
+    plt.savefig('step_response_'+algo+'.eps')
+    ctrl.save('gains_'+algo+'.json')
+
+benchmark('cma', 50, 60)
 
